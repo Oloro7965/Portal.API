cc9f1c35e3332c649fc5e1f8eec569e37207e0f2
Leonardo <leosesh1899@gmail.com>
2025-09-08 16:09:47 -0300

    Criação das pastas restantes (Queries, Update/DeleteCommand, Configuration, Repository e IRepository)

---
+++ Portal.Core/Repositories/IForumRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portal.Core.Repositories
+{
+    internal interface IForumRepository
+    {
+    }
+}
