60dce36feefea6e925a67c6588078137985ae001
natalia973 <nataliasdrf@gmail.com>
2025-09-18 17:57:04 -0300

    Início do Repository na camada de Infrastructure

--- Portal.Infraestructure/Persistance/Repositories/ArtigoRepository.cs
+++ Portal.Infraestructure/Persistance/Repositories/ArtigoRepository.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Portal.Infraestructure.Persistance.Repositories
 {
-    internal class ArtigoRepository
+    public class ArtigoRepository : IArtigoRepository
     {
+        private readonly PortalDbContext _dbContext;
+        public ArtigoRepository(PortalDbContext _dbContext){
+            _dbContext = _dbContext;
+        }
+        public async Task AddAsync(Artigo artigo)
+        { 
+            await_dbcontext.Artigo.AddAsync(room);
+            await_dbcontext.SaveChangeAsync();
+
+        }
+        public async Task<List<Artigo>>GetAllAsync()
+        {
+            return await _dbcontext.Artigo.Where(u => u.IsDeleted.Equals(false)).
+
+        }
+        public async Task<Artigo> GetByIdAsync(Guid id)
+        {
+          return await _dbcontext.Artigo.Where(c=>c.IsDeleted.Equals(false) && c.Id == id)
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _dbcontext.SaveChangesAsync();
+        }
     }
 }
